69bf25139e0355297226ae8fb7b491b90f0c93f9
Xbostick <borovkov-pavel@yandex.ru>
2024-04-09 18:24:55 +0300

    ip change 	modified:   auto_light_up.py

--- auto_light_up.py
+++ auto_light_up.py
@@ -5,6 +5,6 @@ import requests
 while True:
     sleep(60*10)
     if datetime.now().hour + 1 > 22:
-        requests.post("http://192.168.0.107/?auto_power_on=18000000", data="hello_there")
+        requests.post("http://192.168.0.201/?auto_power_on=18000000", data="hello_there")
         sleep(60*60*18)
 
